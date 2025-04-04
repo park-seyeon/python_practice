598cfbe56d032737089ad5b6d8c60bc10329ed6a
seyeonpark11 <88939965+seyeonpark11@users.noreply.github.com>
2025-04-03 21:25:46 -0500

    ex1 character input

---
+++ ex1_character_input.py
@@ -0,0 +1,6 @@
+name = input("My name is ...")
+age = int(input("My age is ..."))
+currentyear = int(input("This year is ..."))
+year = currentyear - age + 100 
+
+print(f"{name} will turn 100 years old in {year}")
